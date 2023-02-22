3a232a4bcc1e7abd28abfdd929d1166666997079
bardzim <bardzim@gmail.com>
2023-02-22 20:14:32 +0400

    error handlers email & password

--- validation-form/src/App.js
+++ validation-form/src/App.js
@@ -3,12 +3,32 @@ import React, { useState } from 'react'
 function App() {
 
     const [email,setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [password, setPassword] = useState('')
     const [emailDirty, setEmailDirty] = useState(false)
     const [passwordDirty, setPasswordDirty] = useState(false)
-    const [emailError, setEmailError] = useState('Email Must be feeled out');
+    const [emailError, setEmailError] = useState('Email Must be feeled out')
     const [passwordError, setPaasswordError] = useState('Password Must be feeled out')
 
+    const emailHandler = (e) => {
+        setEmail(e.target.value)
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i;
+        if (!emailRegex.test(String(e.target.value).toLocaleLowerCase())) {
+            setEmailError('Email is incorrect')
+        } else {
+            setEmailError('')
+        }
+    }
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value)
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@!%*?&])[A-Za-z\d$@!%*?&]{4,}$/;
+        if (!passwordRegex.test(String(e.target.value))) {
+            setPaasswordError('Password is incorrect')
+        } else {
+            setPaasswordError('')
+        }
+    }
+ 
     const blurHandler = (e) => {
         switch (e.target.getAttribute('name') || e.target.getAttribute('password')) {
             case 'email':
@@ -25,9 +45,9 @@ function App() {
             <form>
                 <h1>Registration</h1>
                 {(emailDirty && emailError) && <div style={{color: 'red'}}>{emailError}</div>}
-                <input onBlur={e => blurHandler(e)} name='email' type='text' placeholder='Enter Your Email'/>
+                <input onChange={e => emailHandler(e)} value={email} onBlur={e => blurHandler(e)} name='email' type='text' placeholder='Enter Your Email'/>
                 {(passwordDirty && passwordDirty) && <div style={{color: 'red'}}>{passwordError}</div>}
-                <input onBlur={e => blurHandler(e)} name='password' type='password' placeholder='Enter Your Password'/>
+                <input onChange={e => passwordHandler(e)}  value={password} onBlur={e => blurHandler(e)} name='password' type='password' placeholder='Enter Your Password'/>
                 <button type='submit'>Registration</button>
             </form>
         </div>
